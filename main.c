814c95d72cfee2f48caafeeffa7c959f54cae9db
Aniss-nahim <aniss202nahim@gmail.com>
2019-12-11 02:57:18 +0100

    testing makfile

--- main.c
+++ main.c
@@ -1,4 +1,9 @@
-/**
-    ************* Welcome to the Bulltricker game *****************
-                        copy right 2019
-**/
+/*******************************************************************
+*                 Welcome to the Bulltricker game                  *
+*                        copy right 2019                           *
+********************************************************************/                                                                    
+#include<stdio.h>
+
+void main(){
+    printf("hello world !");
+}
