2ffaa60a8219a7d9dc595f96fc9a098fe1ce1936
ioubnyacs <aubbenyas717@gmail.com>
2020-01-16 21:41:37 +0100

    toto

--- function.h
+++ function.h
@@ -1,21 +1,21 @@
-#include"header.h"
+#include "header.h"
 #define Malloc(type) malloc(sizeof(type))
-#define A(i)    2*i-1 // take number gives a row 
-#define N(i)    2*i-1 //take number gives a col
-#define V(i)    2*(i-1) //take number gives a col
-#define H(i)    2*(i-1) // take number gives a row 
+#define A(i) 2 * i - 1   // take number gives a row
+#define N(i) 2 * i - 1   // take number gives a col
+#define V(i) 2 * (i - 1) // take number gives a col
+#define H(i) 2 * (i - 1) // take number gives a row
 #define PLAYER1 1
 #define PLAYER2 2
-#define PAWN    'P'
-#define DAME    'D'
-#define KING    'K'
+#define PAWN 'P'
+#define DAME 'D'
+#define KING 'K'
 #define PROHIBITED_CELL 'B'
-#define VERTICAL_CELL   'V'
+#define VERTICAL_CELL 'V'
 #define HORIZONTAL_CELL 'H'
-#define ROYAL_CELL      'R'
+#define ROYAL_CELL 'R'
 #define LENGHTBOARD 15
 
-// board initialisation 
+// board initialisation
 void initBoard();
 Piece *initCellObject(int row, int col);
 char getCellType(int row, int col);
@@ -28,7 +28,4 @@ bool isForPawnOrDame(Cell *c);
 bool isForKing(Cell *c);
 //bool isLegaleMove(Move movement, int player);
 
-
-
 void DisplayBoard(Board board);
-
